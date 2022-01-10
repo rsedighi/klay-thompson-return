ad21ad19f17f5a4e4aa60ce4e0ddbe1e3c457934
rsedighi <ramin-og@NL-SF-RAMIN.hsd1.ca.comcast.net>
2022-01-09 17:42:27 -0800

    image updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,7 @@
 						images: {
 							
 							'images/klay-return0.jpeg': 'center',
-							'images/klay-return': 'center',
+							'images/klay-return.jpeg': 'center',
 							'images/klay-return1': 'center',
 							'images/klay-return2.jpeg': 'center',
 							'images/suited-up-klay.jpeg': 'center',
